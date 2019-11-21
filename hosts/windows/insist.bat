e88833e8caf01289b3d1391558b480730d8e85e8
nu11secur1ty <venvaropt@gmail.com>
2019-11-21 13:47:11 +0200

    Update insist.bat

--- hosts/windows/insist.bat
+++ hosts/windows/insist.bat
@@ -2,6 +2,6 @@
 
 set hostspath=%windir%\System32\drivers\etc\hosts
 
-echo 127.0.0.1           facebook.com >> %hostspath%
-echo 127.0.0.1           www.facebook.com >> %hostspath%
+echo 127.0.0.1           facesbook.com >> %hostspath%
+echo 127.0.0.1           www.facesbook.com >> %hostspath%
 exit
