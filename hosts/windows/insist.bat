c91cd80af3498cb63bb4e7c5d11910d8cf9611dd
nu11secur1ty <venvaropt@gmail.com>
2019-11-21 13:38:23 +0200

    Update insist.bat

--- hosts/windows/insist.bat
+++ hosts/windows/insist.bat
@@ -4,5 +4,3 @@ SET NEWLINE=^& echo.
 
 FIND /C /I "ns1.intranet.de" %WINDIR%\system32\drivers\etc\hosts
 IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^0.0.0.0 ns1.intranet.de>>%WINDIR%\System32\drivers\etc\hosts
-
-
