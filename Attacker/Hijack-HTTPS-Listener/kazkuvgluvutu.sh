87e8f6633ee98624d619a22f8ed21122ff87545a
nu11secur1ty <venvaropt@gmail.com>
2019-11-22 16:53:29 +0200

    Create kazkuvgluvutu.sh

---
+++ Attacker/Hijack-HTTPS-Listener/kazkuvgluvutu.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+# Author @nu11secur1ty
+# v-1.0
+echo "";
+echo -e "\e[33mWelcome to *Hijack HTTPS Listener*\e[0m"
+echo -e "\e[32mPress Ctrl + C when you sure have victim already visited your site\e[0m";
+	echo "";
+tail -f /var/log/apache2/access.log
+	exit 0;
